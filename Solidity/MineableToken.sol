95caa0e6748eb889849ae2fa917a59080a2154eb
Mineable Token (0xMT) <133748763+MineableToken@users.noreply.github.com>
2023-05-16 17:14:02 -0500

    Update MineableToken.sol

--- Solidity/MineableToken.sol
+++ Solidity/MineableToken.sol
@@ -42,16 +42,6 @@ interface ArbSys {
 }
 
 
-
-library IsContract {
-    function isContract(address _addr) internal view returns (bool) {
-        bytes32 codehash;
-        /* solium-disable-next-line */
-        assembly { codehash := extcodehash(_addr) }
-        return codehash != bytes32(0) && codehash != bytes32(0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470);
-    }
-}
-
 // File: contracts/utils/SafeMath.sol
 
 library SafeMath2 {
