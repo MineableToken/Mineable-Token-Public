28aeb8e1bfb414d256282d33664c723ef7a21aea
Mineable Token (0xMT) <133748763+MineableToken@users.noreply.github.com>
2023-05-17 22:09:15 -0500

    Update MineableToken.sol

--- Solidity/MineableToken.sol
+++ Solidity/MineableToken.sol
@@ -12,10 +12,10 @@
 // Discord: https://discord.gg/EBHPmMUE45
 //
 // No premine, dev cut, or advantage taken at launch. Public miner available at launch.  
-// 100% of the token is distrubted fairly over 50+ years using Bitcoins model
+// 100% of the token is distrubted fairly over 50+ years using Bitcoins model.
 //
 // **Contract allows 4 days for miners to setup miners with zero rewards**
-//  On GMT: Monday, May 22, 2023 9:29:37 PM the openMining() function is able to be called once to start mining with rewards
+//  On GMT: Monday, May 22, 2023 9:29:37 PM the openMining() function is able to be called once to start mining with rewards.
 //
 // Credits: 0xBitcoin
 
